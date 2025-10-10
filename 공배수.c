cd7f6076a78732c070aa4e08c3bc1afd523aa7cb
janecho1 <abjcd77799@naver.com>
2025-10-10 20:31:59 +0900

    공배수

--- 공배수.c
+++ 공배수.c
@@ -1,21 +1,21 @@
-//#include <stdio.h>
-//#include <stdbool.h>
-//#include <stdlib.h>
-//
-//int solution(int number, int n, int m) {
-//    int answer = 0;
-//    if (number % n == 0 && number % m == 0) {
-//        answer = 1;
-//    }
-//
-//    return answer;
-//}
-//
-//int main() {
-//    int number, n, m;
-//    scanf("%d %d %d", &number, &n, &m);
-//
-//    printf("%d", solution(number, n, m));
-//
-//    return 0;
-//}
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    if (number % n == 0 && number % m == 0) {
+        answer = 1;
+    }
+
+    return answer;
+}
+
+int main() {
+    int number, n, m;
+    scanf("%d %d %d", &number, &n, &m);
+
+    printf("%d", solution(number, n, m));
+
+    return 0;
+}
