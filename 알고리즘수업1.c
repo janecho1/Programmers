7a2f50cb7907b0ecd875fe0ea6ae9cb158cbbb2f
janecho1 <abjcd77799@naver.com>
2025-10-23 23:01:54 +0900

    알고리즘수업1

---
+++ 알고리즘수업1.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//int MenOfPassion(int* A, int n) {
+//    int i = n / 2;
+//
+//    return A[i]; 
+//}
+//
+//int main() {
+//    int n;
+//    int buffer[100];
+//    
+//    scanf("%d", &n);
+//    for (int i = 0; i < 100; i++) {
+//        scanf("%d", buffer[i]);
+//    }
+//
+//    int* A = malloc(strlen(buffer)+1);
+//    strcpy(A, buffer);
+//    int answer = MenOfPassion(A, n);
+//    printf("%d", answer);
+//    free(A);
+//
+//    return 0;
+//}
+int main() {
+	printf("%d\n%d", 1, 0);
+
+	return 0;
+}
