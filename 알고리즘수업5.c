b135bade16403c5f809ee9560f14874eea1a8c0e
janecho1 <abjcd77799@naver.com>
2025-10-27 19:24:52 +0900

    알고리즘수업5

---
+++ 알고리즘수업5.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	unsigned long long n;
+	unsigned long long sum=0;
+	scanf("%lld", &n);
+	 
+		printf("%lld\n%lld", n*n*n, 3);
+
+	return 0;
+}
