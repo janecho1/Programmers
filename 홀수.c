3f2b5bac963aefc5c47129b956148eaa577a9493
조지은 <abjcd77799@naver.com>
2025-12-24 19:26:51 +0900

    홀수

---
+++ 홀수.c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main() {
+//	int sum = 0;
+//	int min= 100;
+//	int i = 0;
+//	int n;
+//	while (i<7) {
+//		i++;
+//		scanf("%d", &n);
+//		if (n%2==1) {
+//			sum += n;
+//			if (min > n) {
+//				min = n;
+//			}
+//		}
+//	}
+//	if (sum == 0) printf("-1");
+//	else printf("%d\n%d", sum, min);
+//
+//	return 0;
+//}
